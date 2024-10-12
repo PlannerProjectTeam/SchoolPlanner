311ea5b88ef0fbafbd81b0b1149ac24e10b58a04
cyrusmccollim <mccollimcyrus@gmail.com>
2024-10-12 18:00:29 -0400

    Initial ReactNative and SpringBoot files and project structure

---
+++ app/backend/src/main/java/com/plannerproject/Main.java
@@ -0,0 +1,10 @@
+package com.plannerproject;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args){
+        System.out.print("Hello world!");
+    }
+}
