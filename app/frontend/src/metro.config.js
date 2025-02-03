f20a9be57170b6b905de9a4c620db8bcf079eb1b
cyrusmccollim <mccollimcyrus@gmail.com>
2025-02-03 01:05:46 -0500

    fix metro configuration

--- app/frontend/src/metro.config.js
+++ app/frontend/src/metro.config.js
@@ -1,26 +1,32 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('metro-config').MetroConfig}
- */
+const defaultConfig = getDefaultConfig(__dirname);
+
+const {
+  resolver: { sourceExts, assetExts },
+} = getDefaultConfig(__dirname);
 
 const config = {
   transformer: {
-    babelTransformerPath: require.resolve('react-native-svg-transformer'),
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
         inlineRequires: true,
       },
     }),
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
   },
   resolver: {
-    assetExts: ['cjs', 'jsx', 'js', 'ts', 'tsx'], // Existing extensions
-    sourceExts: ['svg', 'js', 'jsx', 'ts', 'tsx'], // Add 'svg' here
+    assetExts: assetExts.filter(ext => ext !== 'svg'),
+    sourceExts: [...sourceExts, 'svg'],
   },
 };
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = mergeConfig(defaultConfig, config);
